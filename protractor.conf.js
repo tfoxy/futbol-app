0ad52f8e2c327614219716392522555ef3e49a10
Tomás Fox <tomas.c.fox@gmail.com>
2015-10-15 13:52:05 -0300

    Configurate protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,18 +1,25 @@
 'use strict';
 
-/* global require exports */
+/* global require exports process browser */
 
 const paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
+const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+const hat = require('hat');
+const SpecReporter = require('jasmine-spec-reporter');
 
-// An example configuration file.
-exports.config = {
+
+let config = {
   // The address of a running selenium server.
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   // seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      // When using travis, --no-sandbox arg is pushed to args
+      args: []
+    }
   },
 
   baseUrl: 'http://localhost:3000',
@@ -21,9 +28,41 @@ exports.config = {
   // protractor is called.
   specs: [paths.e2e + '/**/*.js'],
 
+  framework: 'jasmine2',
+
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 30000,
+    // Remove protractor dot reporter
+    print: () => {}
+  },
+
+  onPrepare: () => {
+    if (!process.env.TRAVIS) {
+      jasmine.getEnv().addReporter(new HtmlScreenshotReporter({
+        dest: '.tmp/screenshots',
+        captureOnlyFailedSpecs: true,
+        pathBuilder: function() {
+          return '.' + hat();
+        }
+      }));
+    }
+
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayStacktrace: 'all',
+      displaySpecDuration: true
+    }));
+
+    // Avoid "angular could not be found on the window" error
+    browser.driver.manage().window().maximize();
+    return browser.get('/index.html');
   }
 };
+
+if (process.env.TRAVIS) {
+  config.capabilities.chromeOptions.args.push('--no-sandbox');
+}
+
+exports.config = config;
