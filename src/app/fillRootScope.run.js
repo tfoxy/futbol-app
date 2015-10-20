1df2fd96098278adebff0c0a7d1432623fd49f39
Tomás Fox <tomas.c.fox@gmail.com>
2015-10-20 14:36:18 -0300

    Fix collapse and uncollapse of navbar

--- src/app/fillRootScope.run.js
+++ src/app/fillRootScope.run.js
@@ -1,9 +1,16 @@
-function fillRootScope($rootScope, $state, $stateParams, userData) {
+function fillRootScope($rootScope, $state, $stateParams, userData, $timeout) {
   'ngInject';
 
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
   $rootScope.userData = userData;
+
+  $rootScope.isCollapsed = true;
+  $rootScope.collapse = function collapse() {
+    $timeout(() => {
+      $rootScope.isCollapsed = !$rootScope.isCollapsed;
+    });
+  };
 }
 
 export default fillRootScope;
