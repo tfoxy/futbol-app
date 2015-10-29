85579299541605f4ded1ce0892349df958386f9a
tfoxy <tomas.c.fox@gmail.com>
2015-10-29 15:43:42 -0300

    Add protractor tests

--- e2e/main.po.js
+++ e2e/main.po.js
@@ -5,14 +5,46 @@
 
 'use strict';
 
-function MainPage() {
+const fs = require('fs');
+const mockData = require('./data.mock.json');
 
+const angularMocksScript = fs.readFileSync('bower_components/angular-mocks/angular-mocks.js', {
+  encoding: 'utf8'
+});
+
+function MainPage() {
+  this.nav = {
+    element: $('body > .navbar')
+  };
+  this.nav.standingsEl = this.nav.element.$('a[ui-sref^="app.standings"]');
+  this.nav.resultsTableEl = this.nav.element.$('a[ui-sref^="app.matchesTable"]');
+  this.nav.fixtureEl = this.nav.element.$('a[ui-sref^="app.fixture"]');
+  this.nav.divisionsEls = this.nav.element.$$('[ui-view="divisionList"] a');
 }
 
 MainPage.prototype.navigate = navigate;
+MainPage.prototype.loadMockData = loadMockData;
+MainPage.prototype.logBrowserLog = logBrowserLog;
 
 function navigate() {
   browser.get('/index.html');
+}
+
+function loadMockData() {
+  browser.addMockModule('ngMockE2E', angularMocksScript);
+  browser.addMockModule('futbolAppDev', (mockData) => {
+    angular.module('futbolAppDev', ['ngMockE2E'])
+      .run(['$httpBackend', ($httpBackend) => {
+        $httpBackend.whenGET('assets/data.json').respond(mockData);
+        $httpBackend.whenGET(/^app\//).passThrough();
+      }]);
+  }, mockData);
+}
+
+function logBrowserLog() {
+  browser.manage().logs().get('browser').then(function(browserLog) {
+    console.log('log: ' + require('util').inspect(browserLog));
+  });
 }
 
 module.exports = new MainPage();
