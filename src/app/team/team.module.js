7eb7162208394d35e969fd9ab8c32c4d30eaadaf
Tomás Fox <tomas.c.fox@gmail.com>
2015-11-01 21:19:48 -0300

    Add match modal in team

--- src/app/team/team.module.js
+++ src/app/team/team.module.js
@@ -1,11 +1,13 @@
 import dataModule from '../data/data.module.js';
+import matchModule from '../match/match.module.js';
 import TeamController from './team.controller.js';
 import teamRouteConfig from './team.route.js';
 
 const module = angular.module('futbolApp.team', [
   'ui.router',
   'tableSort',
-  dataModule.name
+  dataModule.name,
+  matchModule.name
 ])
   .config(teamRouteConfig)
   .controller('TeamController', TeamController);
--- src/app/team/team.route.js
+++ src/app/team/team.route.js
@@ -1,14 +1,48 @@
 import TeamController from './team.controller.js';
+import MatchController from '../match/match.controller.js';
 
 function teamRouterConfig($stateProvider) {
   'ngInject';
-
   $stateProvider.state('app.team', {
     url: '/teams/{teamId:[0-9]{1,4}}',
     resolve: TeamController.resolve,
     templateUrl: 'app/team/team.html',
     controller: 'TeamController',
     controllerAs: 'teamCtrl'
+  });
+
+  $stateProvider.state('app.team.match', {
+    url: '/match/{matchId:[0-9]+}',
+    resolve: MatchController.resolve,
+    data: {},
+    onEnter: (ngDialog, $state, match, $timeout) => {
+      'ngInject';
+      function TeamMatchController() {
+        MatchController.call(this, match);
+      }
+      TeamMatchController.prototype = Object.create(MatchController.prototype);
+      TeamMatchController.prototype.constructor = TeamMatchController;
+
+      let dialog = ngDialog.open({
+        template: 'app/match/match.html',
+        controller: TeamMatchController,
+        controllerAs: 'matchCtrl'
+      });
+      dialog.closePromise.then(data => {
+        // If data.value is undefined, it was closed programmatically
+        if ($state.current.name !== 'app.team' && data.value) {
+          $state.go('app.team');
+        }
+      });
+
+      $timeout(() => {
+        $state.current.data.dialog = dialog;
+      });
+    },
+    onExit: $state => {
+      'ngInject';
+      $state.current.data.dialog.close();
+    }
   });
 }
 
