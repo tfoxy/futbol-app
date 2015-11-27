47cca805186441ad24e24d01195dd681d940bfa3
Tomás Fox <tomas.c.fox@gmail.com>
2015-11-27 11:31:57 -0300

    Fix touch on mobile

--- src/app/fillRootScope.run.js
+++ src/app/fillRootScope.run.js
@@ -1,16 +1,27 @@
-function fillRootScope($rootScope, $state, $stateParams, userData, $timeout) {
+function fillRootScope($rootScope, $state, $stateParams, $timeout, $document, userData) {
   'ngInject';
 
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
   $rootScope.userData = userData;
 
+  var isExpandClicked = false;
   $rootScope.isCollapsed = true;
   $rootScope.collapse = function collapse() {
-    $timeout(() => {
-      $rootScope.isCollapsed = !$rootScope.isCollapsed;
-    });
+    console.log('COLLAPSE', $rootScope.isCollapsed);
+    $rootScope.isCollapsed = !$rootScope.isCollapsed;
+    isExpandClicked = true;
   };
+  $document.find('body').on('click', function bodyClickListener() {
+    if (!isExpandClicked) {
+      $rootScope.$apply(function() {
+        console.log('CLICK', $rootScope.isCollapsed);
+        $rootScope.isCollapsed = true;
+      });
+    } else {
+      isExpandClicked = false;
+    }
+  });
 }
 
 export default fillRootScope;
